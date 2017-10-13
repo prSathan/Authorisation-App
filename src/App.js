459330085c34435b3ae9ee49d307ac2875ab1a51
Prabu Sathan <prabu.sathan@gmail.com>
2017-10-13 16:04:14 +0100

    Firebase Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBSrXO9qOYKfAxIg6bmwVcN4EbERGTnI6g',
+      authDomain: 'authentication-faee5.firebaseapp.com',
+      databaseURL: 'https://authentication-faee5.firebaseio.com',
+      projectId: 'authentication-faee5',
+      storageBucket: 'authentication-faee5.appspot.com',
+      messagingSenderId: '305180486979'
+    });
+  }
+
   render() {
     return (
       <View>
